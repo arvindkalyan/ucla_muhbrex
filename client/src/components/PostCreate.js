30baa0cb02f0d3f584e8da33290a25cbeb0338dd
richieej <78634556+richieej@users.noreply.github.com>
2021-11-08 18:39:34 -0800

    added user ID to post creation

--- client/src/components/PostCreate.js
+++ client/src/components/PostCreate.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 function PostCreate() {
     //TODO: populate this field with user from 
     //user authentication 
-    const creator = "sophia"
+    const creator = window.gapi.auth2.getAuthInstance().currentUser.get().getBasicProfile().hT
     const title = useRef()
     const message = useRef()
     
