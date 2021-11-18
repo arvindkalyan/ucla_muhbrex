c36824b5124e7afecab87fe55c89a862db7e8866
Sophia Adrangi <sophiaadrangi@gmail.com>
2021-11-17 20:56:13 -0800

    fixed bug where liking a post would not reflect in display unless you forced a window location update. It now does not require a refresh thank GOD

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -5,26 +5,19 @@ import React, {Component } from 'react'
 class Post extends React.Component{
     constructor(props) {
         super(props)
-        this.state = {
-            creator: this.props.creator,
-            title: this.props.title,
-            message: this.props.message,
-            likes: this.props.likes,
-            id: this.props.id,
-            timeStamp: this.props.timeStamp, 
-        }
+        
         
     }
     
     render() {
         return (
             <div>
-                <p> Creator: {this.state.creator} </p>
-                <p> Title: {this.state.title} </p>
-                <p> Message: {this.state.message} </p>
-                <p> Likes: {this.state.likes} </p>
-                {/* <p> ID: {this.state.id} </p> */}
-                <p> Timestamp: {this.state.timeStamp} </p> 
+                <p> Creator: {this.props.creator} </p>
+                <p> Title: {this.props.title} </p>
+                <p> Message: {this.props.message} </p>
+                <p> Likes: {this.props.likes} </p>
+                {/* <p> ID: {this.props.id} </p> */}
+                <p> Timestamp: {this.props.timeStamp} </p> 
                 <button onClick={() => { this.props.deletePost(this.props.id) }}> Delete </button>
                 <button onClick={() => { this.props.addLike(this.props.id, this.props.likes)}}> Like </button>
             </div>
