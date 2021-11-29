c89bcb8103887089e21c3a70c39fb69ec4b397e8
Arvind Kalyan <arvindkrishnakalyan@gmail.com>
2021-11-29 13:30:03 -0800

    Added icons for like, unlike, delete, edit.
    Working on moving GAuth to the Sidebar.

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -1,6 +1,15 @@
 import axios from 'axios'
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import AddIcon from "@material-ui/icons/Add"
+import { IconButton } from '@material-ui/core'
+import ThumbUpAltOutlinedIcon from '@material-ui/icons/ThumbUpAltOutlined';
+import ThumbUpAltRounded from '@material-ui/icons/ThumbUpAltRounded';
+import ThumbDownAltOutlinedIcon from '@material-ui/icons/ThumbDownAltOutlined';
+import ThumbDownAltRounded from '@material-ui/icons/ThumbDownAltRounded';
+import DeleteOutlineRoundedIcon from '@material-ui/icons/DeleteOutlineRounded'
+
+import EditOutlined from '@material-ui/icons/EditOutlined'
 
 import './Post.css'
 
@@ -38,25 +47,26 @@ class Post extends React.Component{
                 <p> Dislikes: {this.props.dislikes} </p> 
                 {/* <p> ID: {this.props.id} </p> */}
                 <p> Timestamp: {this.props.timeStamp} </p> 
-                {(this.props.userId === this.props.creator) ? <button onClick={(e) => { e.stopPropagation(); this.props.deletePost(this.props.id) }}> Delete </button> : null}
+                
 
                 {/* the following two lines are for like/unlike button */}
-               
+               <div onClick={(e) => {e.stopPropagation()}}>
+                {(this.props.isSignedIn && !this.props.usersLiked.includes(this.props.userId)) ? <IconButton onClick={(e) => { e.stopPropagation();this.props.changeLike(this.props.id, this.props.likes + 1, this.processAddUser(this.props.usersLiked))}}> <ThumbUpAltOutlinedIcon /> </IconButton> : null}
+                {(this.props.isSignedIn && this.props.usersLiked.includes(this.props.userId)) ? <IconButton onClick={(e) => { e.stopPropagation();this.props.changeLike(this.props.id, this.props.likes - 1, this.processRemoveUser(this.props.usersLiked)) }}> <ThumbUpAltRounded /> </IconButton> : null}
                 
-                {(this.props.isSignedIn && !this.props.usersLiked.includes(this.props.userId)) ? <button onClick={(e) => { e.stopPropagation();this.props.changeLike(this.props.id, this.props.likes + 1, this.processAddUser(this.props.usersLiked))}}> Like </button> : null}
-                {(this.props.isSignedIn && this.props.usersLiked.includes(this.props.userId)) ? <button onClick={(e) => { e.stopPropagation();this.props.changeLike(this.props.id, this.props.likes - 1, this.processRemoveUser(this.props.usersLiked)) }}> Unlike </button> : null}
-                
+              
                 {/* the following two lines are for dislike/remove dislike button */}
-                {(this.props.isSignedIn && !this.props.usersDisliked.includes(this.props.userId)) ? <button onClick={(e) => { e.stopPropagation();this.props.changeDislike(this.props.id, this.props.dislikes + 1, this.processAddUser(this.props.usersDisliked))}}> Dislike </button> : null}
-                {(this.props.isSignedIn && this.props.usersDisliked.includes(this.props.userId)) ? <button onClick={(e) => {e.stopPropagation(); this.props.changeDislike(this.props.id, this.props.dislikes - 1, this.processRemoveUser(this.props.usersDisliked)) }}> Remove Dislike </button> : null}
+                {(this.props.isSignedIn && !this.props.usersDisliked.includes(this.props.userId)) ? <IconButton onClick={(e) => { e.stopPropagation();this.props.changeDislike(this.props.id, this.props.dislikes + 1, this.processAddUser(this.props.usersDisliked))}}> <ThumbDownAltOutlinedIcon /> </IconButton> : null}
+                {(this.props.isSignedIn && this.props.usersDisliked.includes(this.props.userId)) ? <IconButton onClick={(e) => {e.stopPropagation(); this.props.changeDislike(this.props.id, this.props.dislikes - 1, this.processRemoveUser(this.props.usersDisliked)) }}> <ThumbDownAltRounded /> </IconButton> : null}
                 
-
                
-                {(this.props.userId === this.props.creator) ? <button onClick={(e) => {
+                {(this.props.userId === this.props.creator) ? <IconButton onClick={(e) => {
                     e.stopPropagation();
                     window.location = '/edit/' + this.props.id
                     //console.log(this.props.id)
-                }}> Edit </button> : null}
+                }}> <EditOutlined/> </IconButton> : null}
+                {(this.props.userId === this.props.creator) ? <IconButton color="secondary" onClick={(e) => { e.stopPropagation(); this.props.deletePost(this.props.id) }}> <DeleteOutlineRoundedIcon/> </IconButton> : null}
+                </div>
             </div>
         )
     }
--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -30,9 +30,10 @@ function Sidebar(props){
           <GoogleAuth />
           {/* <SidebarOption text="UCLA" link="/" Icon={HomeIcon}/>  */}
           <SidebarOption text="Posts" link="/" Icon={Message}/> 
+          
           {props.isSignedIn && <SidebarOption text="Create Post" link="/create" Icon={AddIcon}/>}
           {props.isSignedIn && <SidebarOption text="User" link={`/user/${props.userId}`} Icon={PersonOutline}/>}
-
+          
         </div>
     )
 }
